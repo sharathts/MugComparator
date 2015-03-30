93228d29062892081bb08616f168b7159a036ad9
vasanth kalingeri <vasanth.kalingeri@gmail.com>
2015-03-30 20:26:51 +0530

    Vectorized the mse, running way faster now

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ import numpy as np
 import random
 
 FOLDER = "data"
-LIMIT = 10 #limits the number of images to be considered as part of the database
-SIZE = 150
-TOTAL_NO_IMAGES = 300
+LIMIT = 300 #limits the number of images to be considered as part of the database
+SIZE = 200
+TOTAL_NO_IMAGES = 301
 NO_SIMILAR = 5
 
 NAME = random.sample(range(1, TOTAL_NO_IMAGES), LIMIT)
 NAME = [str(i) for i in NAME]
+cascPath = ''
+#faceCascade = cv2.CascadeClassifier(cascPath)
 
-def mse(x, y, z):
+
+def distance(x, y, z):
     
-    """Find the mean absolute error between two column vectors x and y and add z to this"""
+    """Find the mean absolute error between two column vectors x and y and add z to this. Here z contains the values learnt from the user"""
     
-    return sum([(abs(x[i] - y[i]) + z[i]) for i in range(len(y))])
+    return sum(np.add(abs(np.subtract(x, y)), z))
 
 def preprocess(image):
 
@@ -24,6 +27,7 @@ def preprocess(image):
     global SIZE
     image = cv2.resize(image, (SIZE, SIZE))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
     return image
 
 def normalize_and_flatten(image):
@@ -32,6 +36,7 @@ def normalize_and_flatten(image):
     
     image = image / 255.0 #For now, we apply a very naive method of normalization
     image = image.ravel()
+    image -= image.mean()
     return image
 
 def recognize_image(test_img, data, reinforce_data):
@@ -39,12 +44,13 @@ def recognize_image(test_img, data, reinforce_data):
     """Given the test image return the index of 10 most similar hits"""
     
     global LIMIT
-    #Finds the least mse between test image and the dataset
+    #Finds the least distance between test image and the dataset
     pos = range(LIMIT) # contains the indices of all the images in the database
     value = [] # will contain the mean square errors later
+    
     for i in range(len(pos)):
-        value.append(mse(test_img, data[i], reinforce_data[i]))
-        print i,
+        value.append(distance(test_img, data[i], reinforce_data[i]))
+        print i
         if i == 0:
             continue
         k = i
@@ -55,10 +61,13 @@ def recognize_image(test_img, data, reinforce_data):
             value[k], value[k - 1] = value[k - 1], value[k]
             k -= 1
     print
+    #Now pos will contain the indexes to images in ascending order of distance
     return pos, value
     
 def init():
 
+    """Load the images and the dataset randomly so as to not hamper training"""    
+    
     data = np.zeros(shape=(LIMIT, SIZE*SIZE))
     images = []
     for itr in range(LIMIT):
@@ -72,16 +81,20 @@ def init():
 
 def display_results(images, pos):
     
-    #Now pos will contain the indexes to images in ascending order of mse        
-    for i in range(10):
+    """Displays the most similar images"""
+    
+            
+    for i in range(NO_SIMILAR):
         cv2.imshow(str(i + 1), images[pos[i]])
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 def learn(test_img, data, reinforce_data, value, mismatch_list):
     
+    """Each pixel is penalized accordingly based on the error it made, this penalty is the core of the training process"""
+    
     per_pix_error = value / (SIZE * SIZE)
-    print "learning now", per_pix_error
+#    print "learning now", per_pix_error
     for i in mismatch_list:
         error = [abs(test_img[j] - data[i][j]) for j in range(len(data[i]))]
         error= [per_pix_error / (er + 0.1) for er in error]
@@ -99,17 +112,19 @@ def main():
     test_img = preprocess(test_img)
     cv2.imshow("test", test_img)
     test_img = normalize_and_flatten(test_img)
+    
     reinforce_data = np.zeros(shape=(LIMIT, SIZE*SIZE))
+    
     i = 2
     while i >= 0:
         pos, value = recognize_image(test_img, data, reinforce_data)
         pos = pos[0:NO_SIMILAR]
-        print pos
-        print value
-        value.sort()
-        print value
-        reinforce_data = learn(test_img, data, reinforce_data, max(value), [pos[1]])
-        print "Next iter"
-#        print reinforce_data
+        display_results(images, pos)
+        s = raw_input("Enter numbers of the images which are not similar(space seperated):").split()
+        mismatch_list = [pos[int(x) - 1] for x in s]
+        if len(mismatch_list) == 0:
+            break
+        reinforce_data = learn(test_img, data, reinforce_data, max(value), mismatch_list)
         i -= 1
+        
 main()
